49e2e0701fae8ec6fa9e3b1d91723e105c3eb813
divyabharathi0206 <divyabharathi7h@gmail.com>
2020-01-31 07:56:07 +0530

    test

--- test/src/test/java/test/DragAndDrop.java
+++ test/src/test/java/test/DragAndDrop.java
@@ -8,14 +8,7 @@ import org.openqa.selenium.interactions.Actions;
 
 public class DragAndDrop {
  public static void main(String[] args) {
-	System.setProperty("webdriver.chrome.driver", "C:\\Users\\Divi keerthi\\Desktop\\test\\lib\\chromedriver.exe");
-	WebDriver driver=new ChromeDriver();
-	driver.get("https://www.facebook.com/");
-	WebElement src = driver.findElement(By.id("email"));
-	src.sendKeys("dora");
-	WebElement desc = driver.findElement(By.id("pass"));
-	Actions a=new Actions(driver);
-    a.dragAndDrop(src, desc).perform();
+System.out.println("brach2");
     
 }
 }
